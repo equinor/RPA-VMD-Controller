d9d937e1d788e3f964c452915a69603d5a0633b6
Frode Skjæveland <frode.skj@gmail.com>
2017-11-11 17:39:27 +0100

    Fixed system status page

--- controllers/workflow.js
+++ controllers/workflow.js
@@ -1,49 +1,50 @@
 var ps = require('ps-node');
 var python_shell = require('python-shell');
 
-var PYTHON_WORKER_PROCNAME = 'worker.py'
-var PYTHON_WORKER_PATH = './controllers/python/worker.py'
-var PYTHON_DECIDER_PROCNAME = 'decider.py'
-var PYTHON_DECIDER_PATH = './controllers/python/decider.py'
+var PYTHON_WORKER_PROCNAME = 'worker.py';
+var PYTHON_WORKER_PATH = './controllers/python/worker.py';
+var PYTHON_DECIDER_PROCNAME = 'decider.py';
+var PYTHON_DECIDER_PATH = './controllers/python/decider.py';
 
-console.log(process.cwd());	
-
-
-	
-var decider = null
-var worker = null
-
-var state = {
-	decider: null,
-	worker: null
-}
-
-exports.getStatus = function(){
-	return state;
-}
+var decider = null;
+var worker = null;
 
 exports.start = function start() {
 	checkSystem(PYTHON_WORKER_PROCNAME, function(res) {
 		if (res) {
 			worker = res
-			state.worker = worker;
 		} else {
-			console.log("Start python worker")
+			console.log("Start python worker");
 			startPython(PYTHON_WORKER_PATH)
 		}
-	})
+	});
 	
 	checkSystem(PYTHON_DECIDER_PROCNAME, function(res) {
 		if (res) {
 			decider = res
-			state.decider = decider;
 		} else {
-			console.log("Starting decider")
+			console.log("Starting decider");
 			startPython(PYTHON_DECIDER_PATH)
 		}
 	})
 	
-}
+};
+
+exports.getStatusWorker = function(callback) {
+
+    checkSystem(PYTHON_WORKER_PROCNAME, function (res) {
+        if (res) return callback(res);
+        else return callback(null)
+    })
+};
+
+exports.getStatusDecider = function(callback) {
+
+	checkSystem(PYTHON_DECIDER_PROCNAME, function(res) {
+		if (res) return callback(res);
+        else return callback(null)
+	})
+};
 
 function checkSystem(process_name, callback) {
 	
@@ -51,26 +52,22 @@ function checkSystem(process_name, callback) {
 		command: 'python',
 		arguments: process_name
 		}, function(err, resList) {
-			if(err) {
-				throw new Error(err);
-			}
-			console.log("reslist: " + resList)
-			
-			resList.forEach(function(process) {
-				if(process) return callback(process);
+                if(err) {
+                    throw new Error(err);
+                }
+                // Empty
+                if(resList.length == 0) return callback(null);
+
+                resList.forEach(function(process) {
+                    if(process) return callback(process);
 			});
-			
-			if(resList == '') return callback(null);
-			
+
 	})
 }
-		
 
 function startPython (cmd) {
-	var pshell = python_shell.run(cmd, function (err) {
+    python_shell.run(cmd, function (err) {
 		if(err) throw new Error(err)
 	})
-	
-	
 }
 
--- routes/system.js
+++ routes/system.js
@@ -1,12 +1,58 @@
 var express = require('express');
 var router = express.Router();
-var aws_swf = require('../controllers/aws-swf-controller')
+var workflow = require('../controllers/workflow');
+
+var state = {
+	decider: null,
+	worker: null
+};
+
+var status = {
+    decider : {
+        text: 'Python decider',
+        pid: -1,
+        arg: ''
+    },
+
+    worker: {
+        text: 'Python worker',
+        pid: -1,
+        arg: ''
+    }
+};
 
 
 router.get('/', function(req, res, next) {
 	
-	
-	res.render('index', {id: 'system', selected:'System info'});
+	workflow.getStatusWorker(function (stat) {
+        state.worker = stat;
+
+        if (state.worker) {
+            status.worker.pid = state.worker.pid;
+            status.worker.arg = state.worker.arguments[0];
+        } else // state is null
+        {
+            status.worker.pid = -1;
+            status.worker.arg = '';
+        }
+
+    });
+
+    workflow.getStatusDecider(function (stat) {
+        state.decider = stat;
+
+        if (state.decider) {
+            status.decider.pid = state.decider.pid;
+            status.decider.arg = state.decider.arguments[0];
+        } else // state is null
+        {
+            status.decider.pid = -1;
+            status.decider.arg = ''
+        }
+
+    });
+
+	res.render('index', {id: 'system', selected:'System info', sysStat:status});
 	
 });
 
